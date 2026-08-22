7a4c0a72320bdb35ca64dabc50810934c4cb632a
MarkA <mark2798@outlook.com>
2026-08-21 20:06:44 -0400

    chore: add build data placeholder

---
+++ addon/TalentFetch_Data.lua
@@ -0,0 +1,8 @@
+-- Generated data file. Do not edit by hand.
+-- The production pipeline will replace TalentFetchBuildData with current Wowhead builds.
+
+TalentFetchBuildData = TalentFetchBuildData or {
+    patch = "12.1.0",
+    generatedAt = 0,
+    builds = {},
+}
